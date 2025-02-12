03bb1d62e56e2458ad78affc59ba728f2e369bd7
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2025-02-12 12:31:30 +0530

    Update Product.jsx

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,98 +1,136 @@
+import BurgerImg from '../Components/img/burger.jpg';
+import PizzaImg from '../Components/img/pizza.jpg';
+import ChikengravyImg from '../Components/img/chikengravy.jpg';
+import AppamImg from '../Components/img/appam.jpg';
+import ChapatiImg from '../Components/img/chapati.jpg';
+import ChickenpakodaImg from '../Components/img/chickenpakoda.jpg';
+import Chiken65Img from '../Components/img/chiken65.jpg';
+import ChikenbiriyaniImg from '../Components/img/chikenbiriyani.jpg';
+import ChikennoodlesImg from '../Components/img/chikennoodles.jpg';
+import ChikennugletImg from '../Components/img/chikennuglet.jpg';
+import ChikenwingsImg from '../Components/img/chikenwings.jpg';
+import CurdriceImg from '../Components/img/curdrice.jpg';
+import fishfryImg from '../Components/img/fishfry.jpg';
+import fishgravyImg from '../Components/img/fishgravy.jpg';
+import grilImg from '../Components/img/gril.jpg';
+import honeychikenImg from '../Components/img/honeychiken.jpg';
+import idiyappamImg from '../Components/img/idiyappam.jpg';
+import idlyImg from '../Components/img/idly.jpg';
+import lemonriceImg from '../Components/img/lemonrice.jpg';
+import mealsImg from '../Components/img/meals.jpg';
+import methuvadaImg from '../Components/img/methuvada.jpg';
+import momosImg from '../Components/img/momos.jpg';
+import naanImg from '../Components/img/naan.jpg';
+import paniyaramImg from '../Components/img/paniyaram.jpg';
+import parotaImg from '../Components/img/parota.jpg';
+import podidosaImg from '../Components/img/podidosa.jpg';
+import pongalImg from '../Components/img/pongal.jpg';
+import popcornchikenImg from '../Components/img/popcornchiken.jpg';
+import puriImg from '../Components/img/puri.jpg';
+import ragiidlyImg from '../Components/img/ragiidly.jpg';
+import sambaricebiriyaniImg from '../Components/img/sambaricebiriyani.jpg';
+import tomotoriceImg from '../Components/img/tomotorice.jpg';
+import vadaImg from '../Components/img/vada.jpg';
+import vegpulovImg from '../Components/img/vegpulov.jpg';
+
+
+
+
 export const product = [
     {
       id: 1,
-      img: "../src/Components/img/burger.jpg",
+      img: BurgerImg,
       price: 189,
       name:"Burger",
       alt: "buger",
     },
     {
         id: 2,
-        img: "../src/Components/img/pizza.jpg",
+        img: PizzaImg,
         name:"pizza",
         price: 150,
         alt: "pizza",
       },
       {
         id: 3,
-        img: "../src/Components/img/chikengravy.jpg",
+        img: ChikengravyImg,
         name:"chiken gravy",
         price: 150,
         alt: "chikengravy",
       },
       {
         id: 4,
-        img: "../src/Components/img/appam.jpg",
+        img: AppamImg,
         name:"appam",
         price: 40,
         alt: "appam",
       },
       {
         id: 5,
-        img: "../src/Components/img/chapati.jpg",
+        img: ChapatiImg,
         name:"chapati",
         price: 40,
         alt: "chapati",
       },
       {
         id: 6,
-        img: "../src/Components/img/chickenpakoda.jpg",
+        img: ChickenpakodaImg,
         name:"chicken pakoda",
         price: 120,
         alt: "chickenpakoda",
       },
       {
         id: 7,
-        img: "../src/Components/img/chiken65.jpg",
+        img: Chiken65Img,
         name:"chiken 65",
         price: 130,
         alt: "chiken65",
       },
       {
         id: 8,
-        img: "../src/Components/img/chikenbiriyani.jpg",
+        img: ChikenbiriyaniImg,
         name:"chiken biriyani",
         price: 200,
         alt: "chikenbiriyani",
       },
       {
         id: 9,
-        img: "../src/Components/img/chikennoodles.jpg",
+        img: ChikennoodlesImg,
         name:"chiken noodles",
         price: 150,
         alt: "chikennoodles",
       },
       {
         id: 10,
-        img: "../src/Components/img/chikennuglet.jpg",
+        img: ChikennugletImg,
         name:"chikennuglet",
         price: 190,
         alt: "chikennuglet",
       },
       {
         id: 11,
-        img: "../src/Components/img/chikenwings.jpg",
+        img: ChikenwingsImg,
         name:"chiken wings",
         price: 140,
         alt: "chikenwings",
       },
       {
         id: 12,
-        img: "../src/Components/img/curdrice.jpg",
+        img: CurdriceImg,
         name:"curd rice",
         price: 30,
         alt: "curdrice",
       },
       {
         id: 13,
-        img: "../src/Components/img/fishfry.jpg",
+        img: fishfryImg,
         name:"fish fry",
         price: 150,
         alt: "fishfry",
       },
       {
         id: 14,
-        img: "../src/Components/img/fishgravy.jpg",
+        img: fishgravyImg,
         name:"fish gravy",
         price: 80,
         alt: "fishgravy",
@@ -100,105 +138,105 @@ export const product = [
 
       {
         id: 15,
-        img: "../src/Components/img/gril.jpg",
+        img: grilImg,
         name:"gril",
         price: 150,
         alt: "gril",
       },
       {
         id: 16,
-        img: "../src/Components/img/honeychiken.jpg",
+        img: honeychikenImg,
         name:"honey chiken",
         price: 160,
         alt: "honeychiken",   
        },
       {
         id: 17,
-        img: "../src/Components/img/idiyappam.jpg",
+        img: idiyappamImg,
         name:"idlyappam",
         price: 30,
         alt: "idlyappam",    
       },
       {
         id: 18,
-        img: "../src/Components/img/idly.jpg",
+        img: idlyImg ,
         name:"idly",
         price: 30,
         alt: "idly",   
        },
       {
         id: 19,
-        img: "../src/Components/img/lemonrice.jpg",
+        img: lemonriceImg,
         name:"lemon rice",
         price: 30,
         alt: "lemonrice",   
        },
       {
         id: 20,
-        img: "../src/Components/img/meals.jpg",
+        img: mealsImg,
         name:"meals",
         price: 80,
         alt: "meals",   
        },
       {
         id: 21,
-        img: "../src/Components/img/methuvada.jpg",
+        img: methuvadaImg,
         name:"methuvada",
         price: 10,
         alt: "methuvada",   
        },
       {
         id: 22,
-        img: "../src/Components/img/momos.jpg",
+        img: momosImg,
         name:"momos",
         price: 69,
         alt: "momos",   
        },
       {
         id: 23,
-        img: "../src/Components/img/naan.jpg",
+        img: naanImg,
         name:"naan",
         price: 99,
         alt: "naan",   
        },
       {
         id: 24,
-        img: "../src/Components/img/paniyaram.jpg",
+        img: paniyaramImg,
         name:"paniyaram",
         price: 50,
         alt: "paniyaram",   
        },
       {
         id: 25,
-        img: "../src/Components/img/parota.jpg",
+        img: parotaImg,
         name:"parota",
         price: 35,
         alt: "parota",     
      },
       {
         id: 26,
-        img: "../src/Components/img/podidosa.jpg",
+        img: podidosaImg,
         name:"podi dosa",
         price: 25,
         alt: "podidosa",  
         },
       {
         id: 27,
-        img: "../src/Components/img/pongal.jpg",
+        img: pongalImg,
         name:"pongal",
         price: 30,
         alt: "pongal",
       },
       {
         id: 28,
-        img: "../src/Components/img/popcornchiken.jpg",
+        img: popcornchikenImg,
         name:"popcorn chiken",
         price: 139,
         alt: "popcornchiken",      
       },
       {
         id: 29,
-        img: "../src/Components/img/puri.jpg",
+        img: puriImg,
         name:"puri",
         price: 20,
         alt: "puri",     
@@ -206,35 +244,35 @@ export const product = [
 
       {
         id: 30,
-        img: "../src/Components/img/ragiidly.jpg",
+        img: ragiidlyImg,
         name:"ragi idly",
         price: 40,
         alt: "ragiidly",   
        },
       {
         id: 31,
-        img: "../src/Components/img/sambaricebiriyani.jpg",
+        img: sambaricebiriyaniImg,
         name:"sambarice biriyani",
         price: 230,
         alt: "sambaricebiriyani",
       },
       {
         id: 32,
-        img: "../src/Components/img/tomotorice.jpg",
+        img: tomotoriceImg,
         name:"tomoto rice",
         price: 50,
         alt: "tomotorice",
       },
       {
         id: 33,
-        img: "../src/Components/img/vada.jpg",
+        img: vadaImg,
         name:"vada",
         price: 10,
         alt: "vada",
       },
       {
         id: 34,
-        img: "../src/Components/img/vegpulov.jpg",
+        img: vegpulovImg,
         name:"veg pulov",
         price: 60,
         alt: "vegpulov",
